eea1ec9aa566d48520a9c7691cfe4602a0d506c2
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-11 21:59:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-// server/server.js
 const express = require('express');
 const db = require('./connection/databaseconnection'); // Adjust the path as needed
-const  cors=require('cors');
+const cors = require('cors');
 const app = express();
 // Add this line to parse JSON bodies
 app.use(express.json());
@@ -12,7 +11,7 @@ const port = 3001;
 // ...
 // Example in server.js
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://frontendnew-yeal.onrender.com'],
   credentials: true
 }));
 app.use(function(req, res, next) {
@@ -22,21 +21,17 @@ app.use(function(req, res, next) {
 });
 const usersRoutes = require('./routes/users.js');
 const experiencesRoutes = require('./routes/experiences.js');
-const placementsRoutes= require( './routes/placements.js' );
-const extractQuestions= require('./routes/extract_questions.js')
-app.use('/users',usersRoutes);
-app.use('/experiences',experiencesRoutes);
+const placementsRoutes = require('./routes/placements.js');
+const extractQuestions = require('./routes/extract_questions.js');
+app.use('/users', usersRoutes);
+app.use('/experiences', experiencesRoutes);
 app.use('/placements', placementsRoutes);
 app.use('/extract-questions', extractQuestions);
 app.get('/', (req, res) => {
-    res.send('Hello, this is the root path!');
-  });
-  
+  res.send('Hello, this is the root path!');
+});
 
-
-  
-  // ...
-  
+// ...
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
